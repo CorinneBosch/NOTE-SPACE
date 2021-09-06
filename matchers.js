29d05d367bebc93faf4648be01e99f57db53a4a8
ian k <kickthechair@gmail.com>
2021-09-06 17:08:30 +0100

    toContain test working

--- matchers.js
+++ matchers.js
@@ -1,1 +1,19 @@
 // Branch out and add your matchers here
+
+function expect(actual) {
+  return {
+    toContain: function(expected) {
+      check = actual.includes(expected);
+      if (check === true) {
+        console.log(`${expected} exists in ${actual}`);
+      } else {
+        throw new Error(`${expected} does not exist in ${actual}`)
+      } 
+    }  
+  }
+}
+
+
+  // check 'actual' is in the array by iterating to find the value
+  // return message if value is found
+  // return message if value is not found
