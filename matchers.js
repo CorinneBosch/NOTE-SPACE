5eeba2871131681cc559443bbc36a13916f168cc
Corinne <corinne.boesch@gmail.com>
2021-09-06 18:41:12 +0100

    describe it expect functions working (somehow)

--- matchers.js
+++ matchers.js
@@ -1,19 +1,44 @@
 // Branch out and add your matchers here
 
-function expect(actual) {
-  return {
-    toContain: function(expected) {
-      check = actual.includes(expected);
-      if (check === true) {
-        console.log(`${expected} exists in ${actual}`);
-      } else {
-        throw new Error(`${expected} does not exist in ${actual}`)
-      } 
-    }  
+(function () {
+  var describe_tag = '';
+
+  function describe(description, callback) {
+    describe_tag = description;
+    callback();
   }
-}
 
+  function it(descriotion, callback) {
+    try {
+      callback();
+    } catch (error) {
+      console.log(
+        `${describe_tag || ''}: ${description} failed with error: ${
+          error.message
+        }`
+      );
+    }
+  }
 
-  // check 'actual' is in the array by iterating to find the value
-  // return message if value is found
-  // return message if value is not found
+  function expect(actual) {
+    return {
+      toContain: function (expected) {
+        check = actual.includes(expected);
+        if (check === true) {
+          console.log(`${expected} exists in array`);
+        } else {
+          throw new Error(`${expected} does not exist in array`);
+        }
+      },
+    };
+  }
+
+  describe('Note', () => {
+    it('checks if value is in array', () => {
+      expect([1, 3, 7]).toContain(3);
+    });
+  });
+})();
+// check 'actual' is in the array by iterating to find the value
+// return message if value is found
+// return message if value is not found
