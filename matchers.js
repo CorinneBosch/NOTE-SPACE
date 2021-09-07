355f73aed9d75f6f2ea1e7bc688f4f298447b963
Hotu Nga Morehu Elkington <83406278+ClaimingCookie5@users.noreply.github.com>
2021-09-07 14:03:46 +0100

    Matchers (#2)
    * Adding describe matcher
    
    * toEqual, notToEqual and toHaveSize matchers created
    
    * Refactored
    
    Co-authored-by: Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>

--- matchers.js
+++ matchers.js
@@ -1,5 +1,54 @@
 // Branch out and add your matchers here
 
+(function() {
+  window.describe_block_tag = "";
+
+  function describe (description, callback) {
+    // This is deliberately global
+    window.describe_block_tag = description;
+
+    callback();
+  }
+
+  function expect(actual) {
+    return {
+      toEqual: function (expected) {
+        if (actual === expected) {
+          console.log(".");
+        } else {
+          let block_tag = window.describe_block_tag ? window.describe_block_tag : "";
+          console.log(`${block_tag}: expected ${actual} to equal ${expected}`);
+        }
+      }
+    }
+  }
+})
+
+function expect(actual) {
+  return {
+    toEqual: function (expected) {
+      if(actual === expected) {
+        console.log(".") 
+      } else {
+        throw new Error(`expected ${actual} to equal ${expected}`)
+      }
+    },
+    notToEqual: function (expected) {
+      if(actual !== expected) {
+        console.log(".")
+      } else {
+        throw new Error(`expected ${actual} to equal ${expected}`)
+      }
+    },
+    toHaveSize: function (expected) {
+      if(actual.length === expected) {
+        console.log(".")
+      } else {
+        throw new Error(`expected ${actual} to equal ${expected}`)
+      }
+    }
+  }
+}
 (function () {
   var describe_tag = '';
 
