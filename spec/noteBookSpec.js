65197d5b4b8bd1addd35042546fe8530290871af
ian k <kickthechair@gmail.com>
2021-09-07 16:49:12 +0100

    display full note description & list all notes

--- spec/noteBookSpec.js
+++ spec/noteBookSpec.js
@@ -4,13 +4,35 @@ description = (description) => { console.log(description)};
 emptyArray = (array) => { array = [] }
 const wipeClean = emptyArray(notebook.noteList);
 
+/********************/
+
 expect(notebook.noteList[0]).toEqual(undefined);
+
+/********************/
 
 description("It is expected to be an array")
 expect(notebook.noteList).toBeInstanceOf([]);
 
+/********************/
+
 description("It is expected to save notes to the noteList array");
 notebook.saveNote()
-expect(notebook.noteList).toContain("Note: Yoza");
+expect(notebook.noteList).toContain("If you're visiting this page, you're likely here because you're searching for a random sentence.");
 wipeClean
 
+/********************/
+
+description("It is expected to display the full note");
+notebook.saveNote()
+expect(notebook.fullText(0)).toEqual("If you're visiting this page, you're likely here because you're searching for a random sentence.")
+wipeClean
+
+/********************/
+
+description("It is expected display the abbreviated note");
+notebook.saveNote()
+notebook.saveNote()
+notebook.saveNote()
+notebook.abbreviatedText()
+//console.log(notebook.abbreviatedText());
+//expect(notebook.abbreviatedText()).toEqual("If you're visiting t...");
--- src/noteBook.js
+++ src/noteBook.js
@@ -6,7 +6,18 @@ class NoteBook {
 
   // saveNote will take an argument which will pass it to the new Note class
   saveNote() {
-    this.noteList.push("Note: Yoza");
+    this.noteList.push("If you're visiting this page, you're likely here because you're searching for a random sentence.");
+  }
+
+  fullText(index) {
+    return this.noteList[index] //.description - add this in when calling Note Object
+  }
+
+  abbreviatedText() {
+
+    this.noteList.map((note) => {
+      //console.log(note);
+      note.substring(0, 20)/* + '...' */}) //.description - add this in when calling Note Object
   }
 
 }
