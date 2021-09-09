69b4646bb3901eb58e1a442180ec129852a118ff
Hotu Nga Morehu Elkington <83406278+ClaimingCookie5@users.noreply.github.com>
2021-09-09 14:05:15 +0100

    Interface2 (#8)
    * connected notebook to html and interface
    
    * updates
    
    * updates on interface
    
    * modified the interface file.
    
    * Adding what little I have
    
    * Adding dynamic list to interface
    
    Co-authored-by: Corinne <corinne.boesch@gmail.com>
    Co-authored-by: Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>

--- src/Interface.js
+++ src/Interface.js
@@ -1,1 +1,42 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+
+  updateList = () => {
+    noteBook.abbreviatedText();
+  }
+  const noteBook = new NoteBook();
+
+  console.log(noteBook);
+
+  addListTag = () => {
+    let list = document.querySelector('#list-container');
+    let li = document.createElement('li');
+    let link = document.createElement('a');
+    let noteList = noteBook.abbreviatedText();
+    noteList.forEach(note => {
+      for (let i = 0; i <= noteList.length; i++) {
+        link.href = note;
+        link.innerText = note;
+        li.appendChild(link);
+        list.appendChild(li);
+      }
+    });
+  }
+  
+  updateList();
+  // one route that connects the textarea and passes it as argument of save note
+  document.querySelector('#submit').addEventListener('click', () => {
+    let note = document.querySelector('#create-note').value;
+    noteBook.saveNote(note)
+    addListTag()
+  });
+
+  // returns all elements abbreviated - should be linked to href
+  // document.querySelector('#display-note-list').innerText =
+  //   noteBook.abbreviatedText();
+
+  // parameter should be linked  to empty href of #display-note-list
+  document.querySelector('#display-note-list').addEventListener('click', () => {
+    document.querySelector('#display-full-note').innerText =
+      noteBook.fullText(0);
+  });
+});
