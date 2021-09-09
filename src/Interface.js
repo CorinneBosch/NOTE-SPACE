84dada0f93854bb15dc537a7f3f85a6a5a824ec6
Corinne <corinne.boesch@gmail.com>
2021-09-09 17:05:17 +0100

    implementing api

--- src/Interface.js
+++ src/Interface.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   updateList = () => {
     noteBook.abbreviatedText();
-  }
+  };
   const noteBook = new NoteBook();
 
   console.log(noteBook);
@@ -12,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let li = document.createElement('li');
     let link = document.createElement('a');
     let noteList = noteBook.abbreviatedText();
-    noteList.forEach(note => {
+    noteList.forEach((note) => {
       for (let i = 0; i <= noteList.length; i++) {
         link.setAttribute('id', `${i - 1}`);
         link.href = `#${note}`;
@@ -21,35 +20,58 @@ document.addEventListener('DOMContentLoaded', () => {
         list.appendChild(li);
       }
     });
-  }
-  
+  };
+
   updateList();
 
   // one route that connects the textarea and passes it as argument of save note
   document.querySelector('#submit').addEventListener('click', () => {
     let note = document.querySelector('#create-note').value;
-    noteBook.saveNote(note)
-    addListTag()
-    document.querySelector('#create-note').value = "";
+    noteBook.saveNote(note);
+    addListTag();
+    document.querySelector('#create-note').value = '';
   });
 
   // gets full note from link
-  document.getElementById('list-container').addEventListener('click', (element) => {
-    if (element.target && element.target.nodeName =='A') {
-      console.log(element.target.id)
-      document.getElementById("display-full-note").innerText = noteBook.fullText(element.target.id);
-    }
-  })
+  document
+    .getElementById('list-container')
+    .addEventListener('click', (element) => {
+      if (element.target && element.target.nodeName == 'A') {
+        console.log(element.target.id);
+        document.getElementById('display-full-note').innerText =
+          noteBook.fullText(element.target.id);
+      }
+    });
 
+  const happyEmoji = (text) => {
+    fetch('https://makers-emojify.herokuapp.com/'),
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ text: text }),
+      }
+        .then((response) => response.json())
+        .then((text) => {
+          console.log(text.emojified_text);
+          // document.querySelector('#demoji_test').innerText = note
+        });
+    // .catch(() => {
+    //   // return "status": "ERROR",
+    // }
+  };
 
+  // happyEmoji('Hello, :earth_africa:');
+  // passed to server
+  // -X POST \
+  // -H "Content-Type: application/json" \
+  // -d '{"text": "Hello, :earth_africa:"}'
 
-  // returns all elements abbreviated - should be linked to href
-  // document.querySelector('#display-note-list').innerText =
-  //   noteBook.abbreviatedText();
+  // json resonse
+  // "status": "OK",
+  // "text": "Hello, :earth_africa:",
+  // "emojified_text": "Hello, :earth_africa:"
 
-  // parameter should be linked  to empty href of #display-note-list
-  // document.querySelector('#display-note-list').addEventListener('click', () => {
-  //   document.querySelector('#display-full-note').innerText =
-  //     noteBook.fullText(0);
-  // });
+  // or error response
+  // "status": "ERROR",
+  // "message": "--Error message--"
 });
