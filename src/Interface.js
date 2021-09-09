f800f66817b5bb5edac49abad955388e1bc042eb
Hotu Nga Morehu Elkington <83406278+ClaimingCookie5@users.noreply.github.com>
2021-09-09 16:50:25 +0100

    Abbr note (#9)
    * working on id's
    
    * Adding a return of text on clicking of a link
    
    Co-authored-by: ian k <kickthechair@gmail.com>

--- src/Interface.js
+++ src/Interface.js
@@ -14,7 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     let noteList = noteBook.abbreviatedText();
     noteList.forEach(note => {
       for (let i = 0; i <= noteList.length; i++) {
-        link.href = note;
+        link.setAttribute('id', `${i - 1}`);
+        link.href = `#${note}`;
         link.innerText = note;
         li.appendChild(link);
         list.appendChild(li);
@@ -23,20 +24,32 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   
   updateList();
+
   // one route that connects the textarea and passes it as argument of save note
   document.querySelector('#submit').addEventListener('click', () => {
     let note = document.querySelector('#create-note').value;
     noteBook.saveNote(note)
     addListTag()
+    document.querySelector('#create-note').value = "";
   });
+
+  // gets full note from link
+  document.getElementById('list-container').addEventListener('click', (element) => {
+    if (element.target && element.target.nodeName =='A') {
+      console.log(element.target.id)
+      document.getElementById("display-full-note").innerText = noteBook.fullText(element.target.id);
+    }
+  })
+
+
 
   // returns all elements abbreviated - should be linked to href
   // document.querySelector('#display-note-list').innerText =
   //   noteBook.abbreviatedText();
 
   // parameter should be linked  to empty href of #display-note-list
-  document.querySelector('#display-note-list').addEventListener('click', () => {
-    document.querySelector('#display-full-note').innerText =
-      noteBook.fullText(0);
-  });
+  // document.querySelector('#display-note-list').addEventListener('click', () => {
+  //   document.querySelector('#display-full-note').innerText =
+  //     noteBook.fullText(0);
+  // });
 });
